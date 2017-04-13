c291e493db14519028160fc809588cddab726ecc
chehao <chendong.h.hao@oracle.com>
2017-04-13 09:06:46 +0000

    compile error

--- src/main/java/com/iver99/rest/MyAnnouncementAPI.java
+++ src/main/java/com/iver99/rest/MyAnnouncementAPI.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.ws.rs.Path;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
